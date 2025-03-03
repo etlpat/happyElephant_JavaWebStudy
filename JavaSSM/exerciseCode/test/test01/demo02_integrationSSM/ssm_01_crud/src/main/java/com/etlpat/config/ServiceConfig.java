75ed551bb8bca337c1c56ddb3aa0f4a803269b92
etlpat <wsszw123321@163.com>
2025-03-03 17:15:51 +0800

    2025.03.03

--- JavaSSM/exerciseCode/test/test01/demo02_integrationSSM/ssm_01_crud/src/main/java/com/etlpat/config/ServiceConfig.java
+++
@@ -1,9 +0,0 @@
-package com.etlpat.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.etlpat.service")
-public class ServiceConfig {
-}
